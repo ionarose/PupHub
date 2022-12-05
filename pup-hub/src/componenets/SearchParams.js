42e9b0badb4850d0da29c0fc76c38401ae3fe844
ionarose <ionaroseuk@gmail.com>
2022-12-05 13:41:08 +0000

    custom hook and separation of componenets

--- pup-hub/src/componenets/SearchParams.js
+++ pup-hub/src/componenets/SearchParams.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pets";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -25,7 +25,12 @@ function SearchParams() {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+  onSubmit={e => {
+    e.preventDefault();
+    requestPets();
+  }}
+>
         <label htmlFor="location">
           Location
           <input
@@ -77,14 +82,7 @@ function SearchParams() {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />;
     </div>
   );
 }
---
+++ pup-hub/src/componenets/useBreedList.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+//stores whatever the api has been called for, can also be used so that if you go back to a prev one then no new api call need
+const localCache = {};
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      //if api has no breeds for that animal, will return an empty array so no results
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
