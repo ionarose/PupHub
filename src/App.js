c2b286a212299ddb2565fbc13b902977f401c958
ionarose <ionaroseuk@gmail.com>
2023-02-09 11:29:46 +0000

    styling

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ export default function App(){
               <Link to="/">
               <img src={PupHub} alt="puphub logo" className="logo"/></Link>
             </header>
+            <div class='ripple-background'>
+  <div class='circle xxlarge shade1'></div>
+  <div class='circle xlarge shade2'></div>
+  <div class='circle large shade3'></div>
+  <div class='circle mediun shade4'></div>
+  <div class='circle small shade5'></div>
+</div>
             <Routes>
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
--- src/hooks/fetchBreedList.js
+++ src/hooks/fetchBreedList.js
@@ -8,7 +8,7 @@ async function fetchBreedList({ queryKey }) {
     const res = await fetch(`https://api.petfinder.com/v2/animals?animal=${animal}`, {
       method: "GET",
       mode: "CORS",
-      headers: { Authentication: process.env.BEARER });
+      headers: { Authentication: process.env.BEARER }});
 
     if (!res.ok) {
       throw new Error(`breeds ${animal} fetch not ok`);
